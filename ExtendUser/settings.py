9d6d50192992ff2877e130356cf3d68a1ec30a17
Maximiano Massariol Cardoso <massariolmc@gmail.com>
2020-06-24 19:56:04 -0400

    END PASSWORD CHANGE

--- ExtendUser/settings.py
+++ ExtendUser/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'ExtendUser.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'ExtendUser/templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
